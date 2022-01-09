cbaca557c595cd109dcb35dd668ced761d1e478c
Pemberai Sweto <pembeweb@yahoo.com>
2022-01-09 19:27:20 +0200

    Initial commit

---
+++ udagram-api/src/config/config.ts
@@ -0,0 +1,20 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export const config = {
+  username: `${process.env.POSTGRES_USERNAME}`,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: Number(process.env.DATABASE_PORT),
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
+  aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
+  aws_media_bucket: process.env.AWS_BUCKET,
+  aws_access_key: process.env.AWS_ACCESS_KEY,
+  aws_secret: process.env.AWS_SECRET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
